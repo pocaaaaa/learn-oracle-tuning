6b0e478b81b6bd4fb34590adf8915ecabab458b9
hyunjung <space90220@gmail.com>
2023-08-14 00:40:10 +0900

    1장 SQL 처리 과정과 IO 실습 완료

--- 친절한 SQL 튜닝/1장.SQL 처리 과정과 IO.sql
+++ 친절한 SQL 튜닝/1장.SQL 처리 과정과 IO.sql
@@ -4,6 +4,7 @@ FROM EMP E, DEPT D
 WHERE E.DEPTNO = D.DEPTNO 
 ORDER BY E.ENAME;
 
+
 -- p21 ~ p25
 -- 옵티마이저가 특정 실행계획을 선택하는 근거 테스트 
 CREATE TABLE T 
@@ -13,8 +14,6 @@ FROM EMP E, (SELECT rownum NO FROM DUAL CONNECT BY LEVEL <= 1000) D;
 
 CREATE INDEX t_x01 ON t(deptno, no);
 CREATE INDEX t_x02 ON t(deptno, job, no);
-
-DROP INDEX t_xol;
 
 -- t 테이블 통계정보 수집 
 EXEC dbms_stats.gather_table_stats(user, 't');
@@ -31,3 +30,21 @@ and no = 1;
 SELECT /*+ full(t) */ * FROM t 
 WHERE deptno = 10
 AND NO = 1;
+
+
+-- p41 세그먼트에 할당된 익스텐트 목록 조회 
+SELECT segment_type, tablespace_name, extent_id, file_id, block_id, blocks
+FROM dba_extents
+WHERE owner = USER
+AND segment_name = 'MY_SEGMENT'
+ORDER BY extent_id;
+
+
+-- p45 오라클 데이터베이스의 블록 사이즈를 확인하는 방법
+SQL> show parameter block_size;
+SELECT value FROM v$parameter WHERE name = 'db_block_size';
+
+
+-- p58 오라클에서 손수레에 한 번에 담는 양 => db_file_multiblock_read_count
+SQL> alter session set db_file_multiblock_read_count = 128;
+SQL> show parameter db_file_multiblock_read_count;
