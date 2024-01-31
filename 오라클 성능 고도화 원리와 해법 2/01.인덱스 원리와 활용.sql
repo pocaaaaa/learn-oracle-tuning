43d1aa8b38bb46c18c5db1f611c4e88f8e095624
hyunjung <space90220@gmail.com>
2024-01-31 23:56:23 +0900

    인덱스 원리와 활용 실습 진행 (~p166)

--- 오라클 성능 고도화 원리와 해법 2/01.인덱스 원리와 활용.sql
+++ 오라클 성능 고도화 원리와 해법 2/01.인덱스 원리와 활용.sql
@@ -122,3 +122,141 @@ SELECT /*+ index_combine(e emp_deptno_idx emp_job_idx) */ *
 FROM emp e 
 WHERE deptno = 30
 AND job = 'SALESMAN';
+
+explain plan for
+SELECT /*+ index_combine(e emp_deptno_idx emp_job_idx) */ *
+FROM emp e 
+WHERE deptno = 30 OR job = 'SALESMAN';
+
+SELECT * 
+FROM table(dbms_xplan.display('plan_table', NULL, 'all'));
+
+
+-- Index Join 
+explain plan for
+SELECT /*+ index_join(e EMP_DEPTNO_IDX EMP_JOB_IDX) */ deptno, job 
+FROM emp e 
+WHERE deptno = 30 
+AND job = 'SALESMAN';
+
+
+-- 클러스터링 팩터 
+--SQL> create table t 
+--  2  as 
+--  3  select * from all_objects
+--  4  order by object_id;
+--
+--Table created.
+--
+--SQL> SQL> select count(*) from t;
+--
+--  COUNT(*)
+------------
+--     17782
+--
+--SQL> create index t_object_idx on t(object_id);
+--
+--Index created.
+--
+--SQL> create index t_object_name_idx on t(object_name);
+--
+--Index created.
+--
+--SQL> exec dbms_stats.gather_table_stats(user, 'T');
+--
+--PL/SQL procedure successfully completed.
+--
+--SQL> select i.index_name, t.blocks table_blocks, i.num_rows, i.clustering_factor
+--  2  from user_tables t, user_indexes i
+--  3  where t.table_name = 'T'
+--  4  and i.table_name = t.table_name;
+--
+--INDEX_NAME		       TABLE_BLOCKS   NUM_ROWS CLUSTERING_FACTOR
+-------------------------------- ------------ ---------- -----------------
+--T_OBJECT_NAME_IDX			237	 17782		    7883
+--T_OBJECT_IDX				237	 17782		     237
+
+--SQL> set autotrace traceonly explain 
+--SQL> select /*+ index(t t_object_idx) */ count(*) from t 
+--  2  where object_name >= ' '
+--  3  and object_id > 0;
+--
+--Execution Plan
+------------------------------------------------------------
+--Plan hash value: 2009120224
+--
+-----------------------------------------------------------------------------------------------
+--| Id  | Operation		   	  			| Name	    	| Rows  | Bytes | Cost (%CPU)| Time     |
+-----------------------------------------------------------------------------------------------
+--|   0 | SELECT STATEMENT	     		|		   		|	  1 |	 25 |	278   (1)| 00:00:04 |
+--|   1 |  SORT AGGREGATE 	     		|		   	 	|	  1 |	 25 |			 |		    |
+--|*  2 |   TABLE ACCESS BY INDEX ROWID	| T	 		   	| 17782 |	434K|	278   (1)| 00:00:04 |
+--|*  3 |    INDEX RANGE SCAN	     	| T_OBJECT_IDX 	| 17782 |	    |	 40   (0)| 00:00:01 |
+-----------------------------------------------------------------------------------------------
+--
+--Predicate Information (identified by operation id):
+-----------------------------------------------------
+--   2 - filter("OBJECT_NAME">=' ')
+--   3 - access("OBJECT_ID">0)
+
+--SQL> set autotrace traceonly explain
+--SQL> select /*+ index(t t_object_name_idx) */ count(*) from t 
+--  2  where object_name >= ' '
+--  3  and object_id >= 0; 
+--
+--Execution Plan
+------------------------------------------------------------
+--Plan hash value: 843348828
+--
+----------------------------------------------------------------------------------------------------
+--
+--| Id  | Operation		     			| Name		 		| Rows  | Bytes | Cost(%CPU)| Time	 |
+----------------------------------------------------------------------------------------------------
+--|   0 | SELECT STATEMENT	     		|			 		|     1 |    25 |  7965(1)| 00:01:36 |
+--|   1 |  SORT AGGREGATE 	     		|			 		|     1 |    25 |  	   	  | 	 	 |
+--|*  2 |   TABLE ACCESS BY INDEX ROWID	| T				 	| 17782 |   434K|  7965(1)| 00:01:36 |
+--|*  3 |    INDEX RANGE SCAN	     	| T_OBJECT_NAME_IDX | 17782 |	 	|    76(0)| 00:00:01 |
+----------------------------------------------------------------------------------------------------
+--
+--Predicate Information (identified by operation id):
+-----------------------------------------------------
+--   2 - filter("OBJECT_ID">=0)
+--   3 - access("OBJECT_NAME">=' ')
+
+CREATE TABLE good_cl_factor
+PCTFREE 0 
+AS 
+SELECT t.*, LPAD('x', 630) x FROM SYSTEM.T  
+ORDER BY object_id;
+
+CREATE TABLE bad_cl_factor
+PCTFREE 0 
+AS 
+SELECT t.*, LPAD('x', 630) x FROM SYSTEM.T  
+ORDER BY dbms_random.value;
+
+SELECT table_name, num_rows, blocks, avg_row_len, num_rows/blocks row_per_block
+FROM user_tables
+WHERE table_name IN ('GOOD_CL_FACTOR', 'BAD_CL_FACTOR');
+
+
+-- PK 인덱스에 컬럼 추가 
+explain plan for
+SELECT /*+ ordered use_nl(d) */ *
+FROM emp e, dept d 
+WHERE d.deptno = d.deptno 
+AND d.loc = 'NEW YORK';
+
+SELECT * 
+FROM table(dbms_xplan.display('plan_table', NULL, 'all'));
+
+CREATE INDEX dept_x01 ON dept(deptno, loc);
+
+explain plan for
+SELECT /*+ ordered use_nl(b) rowid(b) */ b.*
+FROM (
+		SELECT /*+ index(emp emp_pk) no_merge */ rowid rid 
+		FROM emp 
+		ORDER BY rowid) a, emp b 
+WHERE b.rowid = a.rid;
+
