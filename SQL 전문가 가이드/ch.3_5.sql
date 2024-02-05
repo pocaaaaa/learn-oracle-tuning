f4d313ec48bc7c64f7bcc7472049fd6315ead464
hyunjung <space90220@gmail.com>
2024-02-05 23:38:14 +0900

    SQL 전문가 가이드 실습 (ch.3_5)

--- SQL 전문가 가이드/ch.3_5.sql
+++ SQL 전문가 가이드/ch.3_5.sql
@@ -32,3 +32,89 @@ WHERE emp.deptno = dept.deptno;
 -- unnest : 서브쿼리를 Unnestin 함으로써 조인방식 최적화 유도 
 -- no_unnest : 서브쿼리를 그대로 둔 상태에서 필터 방식으로 최적화 유도 
 
+-- 조건절 pushdown 
+explain plan FOR
+SELECT deptno, avg_sal 
+FROM (SELECT deptno, avg(sal) avg_sal FROM emp GROUP BY deptno) a 
+WHERE deptno = 30;
+
+explain plan for
+SELECT b.deptno, b.dname, a.avg_sal
+FROM (SELECT deptno, avg(sal) avg_sal FROM emp GROUP BY deptno) a, dept b 
+WHERE a.deptno = b.deptno 
+AND b.deptno = 30;
+
+SELECT b.deptno, b.dname, a.avg_sal
+FROM (SELECT deptno, avg(sal) avg_sal FROM emp GROUP BY deptno) a, dept b 
+WHERE a.deptno = b.deptno 
+AND b.deptno = 30
+AND a.deptno = 30;
+
+SELECT * 
+FROM table(dbms_xplan.display);
+
+-- 조건절 Pullup 
+explain plan for
+SELECT * FROM 
+(SELECT deptno, avg(sal) FROM emp WHERE deptno = 10 GROUP BY deptno) e1, 
+(SELECT deptno, min(sal), max(sal) FROM emp GROUP BY deptno) e2 
+WHERE e1.deptno = e2.deptno;
+
+SELECT * FROM 
+(SELECT deptno, avg(sal) FROM emp WHERE deptno = 10 GROUP BY deptno) e1, 
+(SELECT deptno, min(sal), max(sal) FROM emp WHERE deptno = 10 GROUP BY deptno) e2 
+WHERE e1.deptno = e2.deptno;
+
+SELECT * FROM table(dbms_xplan.display);
+
+-- 조인 조건 pushdown
+explain plan for
+SELECT d.deptno, d.dname, e.avg_sal
+FROM dept d,
+	 (SELECT /*+ no_merge push_pred */ deptno, avg(sal) avg_sal FROM emp GROUP BY deptno) e 
+WHERE e.deptno (+)= d.deptno; 
+
+SELECT d.deptno, d.dname
+	 , (SELECT avg(sal) FROM emp WHERE deptno = d.deptno) avg_sal
+	 , (SELECT min(sal) FROM emp WHERE deptno = d.deptno) min_sal 
+	 , (SELECT max(sal) FROM emp WHERE deptno = d.deptno) max_sal 
+FROM dept d;
+
+SELECT deptno, dname 
+	 , to_number(substr(sal, 1, 7)) avg_sal
+	 , to_number(substr(sal, 8, 7)) min_sal 
+	 , to_number(substr(sal, 15)) max_sal 
+FROM (
+	SELECT /*+ no_merge */ d.deptno, d.dname
+		 , (SELECT lpad(avg(sal), 7) || lpad(min(Sal), 7) || max(sal)
+		 	FROM emp WHERE deptno = d.deptno) sal 
+	FROM dept d
+);
+
+-- 조건절 이행 
+explain plan for
+SELECT * FROM dept d, emp e 
+WHERE e.job = 'MANAGER'
+AND e.deptno = 10
+AND d.deptno = e.deptno;
+
+SELECT * FROM dept d, emp e 
+WHERE e.job = 'MANAGER'
+AND e.deptno = 10
+AND d.deptno = 10;
+
+-- OR 조건을 Union으로 변환
+explain plan FOR 
+SELECT * FROM emp 
+WHERE job = 'CLERK' OR deptno = 20;
+
+SELECT * FROM table(dbms_xplan.display);
+
+explain plan FOR 
+SELECT /*+ use_concat */ * FROM emp 
+WHERE job = 'CLERK' OR deptno = 20;
+
+explain plan FOR 
+SELECT /*+ no_expand */ * FROM emp 
+WHERE job = 'CLERK' OR deptno = 20;
+
