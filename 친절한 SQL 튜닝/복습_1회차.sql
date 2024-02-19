e874ca40abd8e016acae0b67cae95498a4ba93f4
hyunjung <space90220@gmail.com>
2024-02-20 00:22:30 +0900

    복습 1회차 (~3.2장)

--- 친절한 SQL 튜닝/복습_1회차.sql
+++ 친절한 SQL 튜닝/복습_1회차.sql
@@ -73,3 +73,54 @@ SELECT * FROM emp WHERE sal > 2000 ORDER BY ename;
 
 explain plan FOR
 SELECT * FROM emp WHERE empno > 0 ORDER BY empno DESC;
+
+
+-- 3장 
+-- insert into 고객_임시
+-- select /*+ full(c) full(h) index_ffs(m.고객변경이력) ordered no_merge(m) use_hash(m) use_hash(h) */ 
+-- 		  c.고객번호, c.고객명, h.전화번호, h.주소, h.상태코드, h.변경일시
+-- from 고객 c 
+-- 		, (select 고객번호, max(변경일시) 최종변경일시 
+-- 		   from 고객변경이력 
+-- 		   where 변경일시 >= trunc(add_months(sysdate, -12), 'mm')
+--		   and 변경일시 < trunc(sysdate, 'mm)
+--		   group by 고객번호) m 
+-- 		, 고객변경이력 h 
+-- where c.고객구분코드 = 'A001'
+-- and m.고객번호 = c.고객번호
+-- and h.고객번호 = m.고객번호 
+-- and h.변경일시 = m.최종변경일시 
+
+-- insert into 고객_임시
+-- select 고객번호, 고객명, 전화번호, 주소, 상태코드, 변경일시
+-- from (select /*+ full(c) full(h) leading(c) use_hash(h) */ 
+--				c.고객번호, c.고객명, h.전화번호, h.주소, h.상태코드, h.변경일시
+--				, rank() over(partition by h.고객번호 order by h.변경일시 desc) no 
+--		 from 고객 c, 고객변경이력 h
+--		 where c.고객구분코드 = 'A001'
+--		 and h.변경일시 >= trunc(add_months(sysdate, -12), 'mm')
+--		 and h.변경일시 < trunc(sysdate, 'mm')
+--		 and h.고객번호 = c.고객번호) 
+-- where no = 1
+
+-- create table index_org_t (a number, b varchar(10), constraint index_org_t_pk primary key (a) )
+-- organization index; 
+
+-- create table heap_org_t (a number, b varchar(10), constraint heap_org_t_pk primary key (a) )
+-- organization heap; 
+
+-- create cluster c_dept#(deptno number(2)) index;
+-- create index c_dept#_idx on cluster c_dept#;
+
+-- create table dept (
+-- 		deptno number(2) not null
+--		, dname varchar2(14) not null
+--		, loc varchar2(13) )
+-- cluster c_dept#(deptno); 
+
+-- create cluster c_dept# (deptno number(2)) hashkeys 4;
+-- create table dept (
+-- 		deptno number(2) not null
+--		, dname varchar2(14) not null
+--		, loc varchar2(13) )
+-- cluster c_dept#(deptno); 
